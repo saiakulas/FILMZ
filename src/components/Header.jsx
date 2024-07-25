e7a93008c80f93dd741c9588179f9a8dfbb87288
sai akula <saiakula79989@gmail.com>
2024-07-25 18:13:14 +0530

    modes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {AiFillHome} from 'react-icons/ai' 
 import {BsFillInfoCircleFill} from 'react-icons/bs'
 import Menu from './Menu'
+import Darkmode from './Darkmode'
 function Header() {
   return (
-    <div>
-    <div className="flex gap-4">
+    <div className=''>
+    <div className="flex gap-4 pl-4 mb-4">
         <Menu title="home" address="/" Icon={AiFillHome}/>
         <Menu title="about" address="/about" Icon={BsFillInfoCircleFill}/>
+    </div>
+    <div className='flex item-center gap-4'>
+      <Darkmode/>
     </div>
       <div className="absolute top-0 right-0 p-4">
        <span className='text-xl hidden sm:inline bg-amber-200 rounded '>FILMZ</span>
